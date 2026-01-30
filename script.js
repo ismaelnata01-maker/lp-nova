753524c4cfddc166bfe7d2392071d4aac1b667ca
ismaelnata01-maker <ismaelnata01@gmail.com>
2026-01-30 15:40:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function enviar(){
     const assunto = document.getElementById('assunto').value
     const mensagem = `Gostaria de entrar em contato!\n\nNome: ${nome} \nAssunto: ${assunto}`
     const msg = encodeURIComponent(mensagem)
-    window.open(`https://wa.me/5541997274520?text=${msg}`)
+    window.open(`https://wa.me/5541*********?text=${msg}`)
 
 }
